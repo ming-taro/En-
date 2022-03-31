489a251fd7cf94d5d8f2bfa5da501062020fa411
ming-taro <hera002008@naver.com>
2022-03-31 19:23:44 +0900

    별찍기 파일

---
+++ PrintStar/PrintStar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PrintStar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int k=0; k<2*i+1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            /*for(int i=n; i<n; i++)
+            {
+                for(int j=0; j<; j++)
+                {
+
+                }
+            }*/
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
