f6c2f95e88830a7ef748ed561f7c40d542e0593f
ming-taro <hera002008@naver.com>
2022-04-03 14:35:53 +0900

    숫자범위가 1~4인지 판별하는 함수 작성

--- PrintStar/PrintStar/ExceptionHandling.cs
+++ PrintStar/PrintStar/ExceptionHandling.cs
@@ -8,11 +8,16 @@ namespace PrintStar
 {
     class ExceptionHandling
     {
-        public bool IsStringLengthOne(String str)
+        public bool IsStringLengthOne(String str)    //문자열 길이가 1인지 이상인지 판별
         {
             if (str.Length == 1) return true;
             else return false;
         }
 
+        public bool IsBetween1To4(int number)       //숫자가 1~4범위인지 판별(메뉴선택시 사용할 예외처리)
+        {
+            if (number >= 1 && number <= 4) return true;
+            else return false;
+        }
     }
 }
