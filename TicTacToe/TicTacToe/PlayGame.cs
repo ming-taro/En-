771a4c1773650355af7cdd1e976122d83aa17b6e
ming-taro <hera002008@naver.com>
2022-04-04 01:02:47 +0900

    [ADD] 유저vs컴퓨터모드 실행 함수 작성 시작, 유저좌표 입력함수 추가

--- TicTacToe/TicTacToe/PlayGame.cs
+++ TicTacToe/TicTacToe/PlayGame.cs
@@ -6,27 +6,59 @@ using System.Threading.Tasks;
 
 namespace TicTacToe
 {
-    class PlayGame
+    class PlayGame  
     {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Board board = new Board();
+
+        public void InputUserPoint(char drawType)
+        {
+            Console.WriteLine(">>>>>>>>좌표를 입력해주세요<<<<<<<<");
+            Console.Write(">행 : ");
+            string input = Console.ReadLine();
+            int row = Convert.ToInt32(input);
+            Console.Write(">열 : ");
+            input = Console.ReadLine();
+            int column = Convert.ToInt32(input);
+
+            board.SetOneSpace(row, column, drawType);
+        }
+
+        
+        public void UserVersusComputer()
+        {
+            bool loop = true;
+            
+            player1.DrawType = 'X';    //user
+            player2.DrawType = 'O';    //computer
+
+            while (loop)
+            {
+                InputUserPoint(player1.DrawType);   //유저 입력
+            }
+
+
+        }
+
         public void StartGame()
         {
             PrintScreen printScreen = new PrintScreen();
             printScreen.PrintMainScreen();
 
-            Player player1 = new Player();
-            Player player2 = new Player();
-
-            Board board = new Board();
-
             int mode = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
             printScreen.PrintModeScreen(mode);
             printScreen.PrintScoreBoard(mode, 0, 0);
-
-            
-
-
             printScreen.PrintBoardScreen();
+
+            bool loop = true;
+
+            while (loop)
+            {
+
+
+            }
         }
 
 
