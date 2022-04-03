238ef1bc4b8eb52e3df10847a633d1e7b77357e6
ming-taro <hera002008@naver.com>
2022-04-03 15:43:13 +0900

    StartGame클래스에 InputMenu()함수를 추가해 메뉴를 입력받고 예외사항을 처리함

--- PrintStar/PrintStar/StartGame.cs
+++ PrintStar/PrintStar/StartGame.cs
@@ -9,7 +9,7 @@ namespace PrintStar
     {
         public void ShowMenu()
         {
-            Console.Clear();
+            
             Console.WriteLine(">>>>>>>>>>>>>>>>>>>>별찍기 프로그램<<<<<<<<<<<<<<<<<<<<<\n\n");
             Console.WriteLine("                *                       ***");
             Console.WriteLine("               ***                       *\n");
@@ -19,6 +19,29 @@ namespace PrintStar
             Console.WriteLine("               ***                       *\n");
             Console.WriteLine("          3. 모래시계              4.  다이아\n\n");
             Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
+
+        }
+
+        public int InputMenu()
+        {
+            ExceptionHandling exception = new ExceptionHandling();
+
+            String str = Console.ReadLine();
+
+            if (exception.IsEnter(str) || exception.IsStartSpace(str) || exception.IsStringLengthOne(str) == false)
+            {
+                Console.WriteLine(">>>>>잘못된 입력입니다. 숫자를 입력해주세요.<<<<<\n");
+                return -1;
+            }
+            else if(exception.IsBetween1To4(Convert.ToInt32(str)) == false)
+            {
+                Console.WriteLine(">>>>>잘못된 입력입니다. 1~4 중 하나를 입력해주세요.<<<<<\n");
+                return -1;
+            }
+            else
+            {
+                return Convert.ToInt32(str);
+            }
 
         }
 
@@ -53,24 +76,29 @@ namespace PrintStar
 
         public void PlayGame()
         {
-            int menu;
-            int line;
-
             Console.SetWindowSize(100, 45);
 
-            while (true)
-            {
-                ShowMenu();    //메뉴화면을 보여줌
-                Console.Write(">>>번호를 입력해주세요 : ");             //찍으려는 별 모양 고르기
-                menu = Convert.ToInt32(Console.ReadLine());
+            int menu;
+            int line;
+            bool loop = true;
 
-                Console.Write(">>>줄 수를 입력해주세요 : ");   //찍으려는 별의 줄 수 입력
+            ShowMenu();    //메뉴화면을 보여줌
+
+            while (loop)
+            {
+                Console.Write(">번호를 입력해주세요 : ");             //찍으려는 별 모양 고르기
+                menu = InputMenu();
+                if (menu == -1) continue;          //예외상황 발생시 처음으로 다시
+
+                Console.Write(">줄 수를 입력해주세요 : ");   //찍으려는 별의 줄 수 입력
                 line = Convert.ToInt32(Console.ReadLine());
 
                 SelectMenu(menu, line); //메뉴선택 후 화면에 해당 메뉴를 보여줌
 
-                if (isRetry()) continue;  //게임 다시 시작
-                else break;  //
+                if (isRetry() == false) break;       //게임종료
+
+                Console.Clear();
+                ShowMenu();    //메뉴화면을 보여줌
             }
 
             Console.WriteLine("게임을 종료합니다...");
