2eae6384a6865c2f87951b982f1e14903b7968f5
ming-taro <hera002008@naver.com>
2022-04-03 14:20:53 +0900

    예외처리클래스 생성, 입력받은 문자열 길이가 1인지 판별하는 함수 작성함

---
+++ PrintStar/PrintStar/ExceptionHandling.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintStar
+{
+    class ExceptionHandling
+    {
+        public bool IsStringLengthOne(String str)
+        {
+            if (str.Length == 1) return true;
+            else return false;
+        }
+
+    }
+}
