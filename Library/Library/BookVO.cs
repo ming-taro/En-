72d26337ebd707c7374f03347bab44df4de31099
ming-taro <hera002008@naver.com>
2022-04-08 00:05:48 +0900

    [ADD] BookVo 생성

--- Library/Library/BookVO.cs
+++ Library/Library/BookVO.cs
@@ -8,5 +8,26 @@ namespace Library
 {
     class BookVO
     {
+        private string id;            //도서번호
+        private string name;          //도서명
+        private string publisher;     //출판사
+        private string author;        //저자
+        private string price;         //가격
+        private string quantity;      //수량
+
+        public BookVO(string id, string name, string publisher, string author, string price, string quantity)
+        {
+            this.id = id;
+            this.name = name;
+            this.publisher = publisher;
+            this.author = author;
+            this.price = price;
+            this.quantity = quantity;
+        }
+        public override string ToString()
+        {
+            return "도서번호: " + id + "\n도서명" + name + "\n출판사" + publisher +
+                "\n저자: " + author + "\n가격: " + price + "\n수량: " + quantity;
+        }
     }
 }
