e40416f92873dcfc7129d4838b21c47f9c019994
ming-taro <hera002008@naver.com>
2022-04-14 21:33:53 +0900

    [FIX] 키보드 클래스에서 left이동시 top이 움직이는 오류 수정

--- TimeTable/TimeTable/Controller/Keyboard.cs
+++ TimeTable/TimeTable/Controller/Keyboard.cs
@@ -42,7 +42,7 @@ namespace TimeTable
             while (Constants.KEYBOARD_OPERATION)    //키보드 방향키로 움직이는 동안
             {
                 Console.SetCursorPosition(left, top);
-                key = ControlKeyboard(move);            //키보드를 입력받음
+                key = ControlKeyboardUpAndDown(move);            //키보드를 입력받음
                 if (OutOfTop(minTop, maxTop) == (int)Constants.Keyboard.OUT_OF_MENU) continue;  //메뉴를 벗어나는 이동은 X
 
                 //메뉴입력 -> 해당 메뉴로 이동
@@ -58,7 +58,7 @@ namespace TimeTable
             while (Constants.KEYBOARD_OPERATION)
             {
                 Console.SetCursorPosition(left, top);
-                key = ControlKeyboard(move);            //키보드를 입력받음
+                key = ControlKeyboardLeftAndRight(move);            //키보드를 입력받음
                 if (OutOfLeft(minLeft, maxLeft) == (int)Constants.Keyboard.OUT_OF_MENU) continue;  //메뉴를 벗어나는 이동은 X
 
                 //메뉴입력 -> 해당 메뉴로 이동
@@ -67,7 +67,7 @@ namespace TimeTable
 
             return key;
         }
-        public int ControlKeyboard(int move) //현재 키보드 입력값 반환
+        public int ControlKeyboardUpAndDown(int move) //현재 키보드 입력값 반환
         {
             ConsoleKeyInfo keyInfo;
 
@@ -91,6 +91,30 @@ namespace TimeTable
                 }
             }
         }
+        public int ControlKeyboardLeftAndRight(int move) //현재 키보드 입력값 반환
+        {
+            ConsoleKeyInfo keyInfo;
+
+            while (Constants.KEYBOARD_OPERATION)   //다른 키를 입력하면 올바른 키를 입력할때까지 무한루프
+            {
+                keyInfo = Console.ReadKey(true);   //키를 입력받음 
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        left += move;
+                        Console.SetCursorPosition(left, top);     //→방향키 입력
+                        return (int)Constants.Keyboard.MOVING_CURSOR;
+                    case ConsoleKey.LeftArrow:                    //←방향키 입력
+                        left -= move;
+                        Console.SetCursorPosition(left, top);
+                        return (int)Constants.Keyboard.MOVING_CURSOR;
+                    case ConsoleKey.Enter:                        //enter 입력
+                        return (int)Constants.Keyboard.ENTERING_MENU;
+                    case ConsoleKey.Escape:                       //escape 입력
+                        return (int)Constants.Keyboard.ESCAPE;
+                }
+            }
+        }
         public int OutOfTop(int minTop, int maxTop)
         {
             if (top < minTop) top = minTop;
@@ -101,8 +125,8 @@ namespace TimeTable
         }
         public int OutOfLeft(int minLeft, int maxLeft)
         {
-            if (Left < minLeft) top = minLeft;
-            else if (Left > maxLeft) top = maxLeft;
+            if (Left < minLeft) left = minLeft;
+            else if (Left > maxLeft) left = maxLeft;
             else return (int)Constants.Keyboard.WITHIN_THE_MENU;  //입력범위 내에서 키보드를 움직임
 
             return (int)Constants.Keyboard.OUT_OF_MENU;   //입력 범위를 벗어남
