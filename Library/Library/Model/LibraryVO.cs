cb5cf2fd1fcf62c15605007c72c06dfedd5c969c
ming-taro <hera002008@naver.com>
2022-04-19 14:32:32 +0900

    [MAKE] LibraryVO에 겹치는 코드 함수로 빼둠

--- Library/Library/Model/LibraryVO.cs
+++ Library/Library/Model/LibraryVO.cs
@@ -30,7 +30,7 @@ namespace Library
         {
             try
             {
-                connection = new MySqlConnection("Server=localhost;Port=3306;Database=booklist;Uid=root;Pwd=root");
+                connection = new MySqlConnection("Server=localhost;Port=3306;Database=booklist;Uid=root;Pwd=0000");
                 connection.Open();
             }
             catch(Exception e)
@@ -51,70 +51,30 @@ namespace Library
             }
         }
    
-        
         public void InsertBookList(string id, string name, string publisher, string author, string price, string quantity)  //책목록
         {
-            try
-            {
-                string sql = "INSERT INTO booklist VALUES (" + id + "," + name + "," + publisher + "," + author + "," + price + "," + quantity + ");";
-                MySqlCommand command = new MySqlCommand(sql, connection);
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("실패");
-                Console.WriteLine(ex.ToString());
-            }
-
+            StartQuery("INSERT INTO booklist VALUES ('" + id + "','" + name + "','" + publisher + "','" + author + "','" + price + "'," + quantity + ");");
         }
         public void InsertMember(string id, string password, string name, string age, string phoneNumber, string address)
         {
-            try
-            {
-                string sql = "INSERT INTO member VALUES (\""+id +"\",\""+password+"\",\""+name+"\",\""+age+"\",\""+phoneNumber+"\",\""+address+"\");";
-                MySqlCommand command = new MySqlCommand(sql, connection);
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("실패");
-                Console.WriteLine(ex.ToString());
-            }
-
+            StartQuery("INSERT INTO member VALUES ('" + id + "','" + password + "','" + name + "','" + age + "','" + phoneNumber + "','" + address + "');");
         }
         public void InsertBorrowBook(string memberId, string bookId)
         {
-            try
-            {
-                string sql = "INSERT INTO borrowBook(memberId,bookId) VALUES ('" + memberId + "', '" + bookId + "');";
-                MySqlCommand command = new MySqlCommand(sql, connection);
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("실패");
-                Console.WriteLine(ex.ToString());
-            }
+            StartQuery("INSERT INTO borrowBook(memberId,bookId) VALUES ('" + memberId + "', '" + bookId + "');");
         }
         public void DeleteBorrowBook(string memberId, string bookId)
         {
-            try
-            {
-                string sql = "DELETE FROM borrowBook WHERE memberId='" + memberId + "' and bookId='" + bookId + "';";
-                MySqlCommand command = new MySqlCommand(sql, connection);
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("실패");
-                Console.WriteLine(ex.ToString());
-            }
+            StartQuery("DELETE FROM borrowBook WHERE memberId='" + memberId + "' and bookId='" + bookId + "';");
         }
         public void UpdateQuantity(string bookId)
         {
+            StartQuery("UPDATE book SET quantity = quantity + 1 WHERE id='" + bookId + "';");
+        }
+        public void StartQuery(string sql)
+        {
             try
             {
-                string sql = "UPDATE book SET quantity = quantity + 1 WHERE id='" + bookId + "';";
                 MySqlCommand command = new MySqlCommand(sql, connection);
                 command.ExecuteNonQuery();
             }
@@ -123,32 +83,6 @@ namespace Library
                 Console.WriteLine("실패");
                 Console.WriteLine(ex.ToString());
             }
-
         }
-        /*public void SetMemberList()  //회원목록
-        {
-            using (MySqlConnection connection = new MySqlConnection("Server=localhost;Port=3306;Database=booklist;Uid=root;Pwd=root"))
-            {
-                try//예외 처리
-                {
-                    connection.Open();
-                    string sql = "select*from member";
-                    MySqlCommand command = new MySqlCommand(sql, connection);
-                    MySqlDataReader table = command.ExecuteReader();
-
-                    while (table.Read())
-                    {
-                        memberList.Add(new MemberVO(table["id"].ToString(), table["password"].ToString(), table["name"].ToString(), table["age"].ToString(), table["phoneNumber"].ToString(), table["address"].ToString()));
-                    }
-                    table.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("실패");
-                    Console.WriteLine(ex.ToString());
-                }
-
-            }
-        }*/
     }
 }
