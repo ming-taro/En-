0e91413c98fbfb88d102af2d03f8b0de3097ee78
ming-taro <hera002008@naver.com>
2022-05-03 03:22:43 +0900

    [ADD] 로그관리 클래스 추가

---
+++ Library/Library/Model/LogDAO.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class LogDAO
+    {
+        private static LogDAO logDAO;
+        public static LogDAO GetInstance()
+        {
+            if (logDAO == null)
+            {
+                logDAO = new LogDAO();
+            }
+            return logDAO;
+        }
+        public void AddToRentalList(MySqlCommand command, MySqlConnection connection)
+        {
+            command = new MySqlCommand(Constants.DECREASE_IN_BOOK_QUANTITY, connection); 
+            command.Parameters.Add(new MySqlParameter("@bookId", bookId));
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ Library/Library/Model/LogVO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class LogVO
+    {
+        private string id;
+        private string user;
+        private string menu;
+        private string content;
+        private string date;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+        public string Menu
+        {
+            get { return menu; }
+            set { menu = value; }
+        }
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+    }
+}
