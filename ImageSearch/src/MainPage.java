5c552ba253814cf675ce6c6f8d27b1f238878452
ming-taro <hera002008@naver.com>
2022-05-08 22:57:04 +0900

    [ADD] 검색어 입려필드, 버튼 추가

--- ImageSearch/src/MainPage.java
+++ ImageSearch/src/MainPage.java
@@ -6,18 +6,30 @@ public class MainPage extends JFrame {
 
 		setTitle("Image Search");
 		setBounds(450, 150, 800, 600);
-		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		add(panel);
+		Font font = new Font("돋움", Font.BOLD, 20);
+		
+		JPanel searchPanel = new JPanel();
+		searchPanel.setLayout(null);
+		add(searchPanel);
+		
+		JTextField searchField = new JTextField("검색어 입력");   //검색어 입력창
+		searchField.setBounds(150, 270, 450, 50);
+		searchField.setFont(font);
+		
+		JButton searchButton = new JButton();
+		searchButton.setBounds(600, 270, 50, 50);
 		
 		JButton button = new JButton("검색 기록");
-		button.setBounds(620, 500, 130, 50);
-		button.setFont(new Font("돋움", Font.BOLD, 20));
-		panel.add(button);
+		button.setBounds(620, 480, 130, 40);
+		button.setFont(font);
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		searchPanel.add(button);
+		searchPanel.add(searchField);
+		searchPanel.add(searchButton);
+		
+		setVisible(true);
 	}
 
 }
