c88a95d29278baa9504b2203ddf885ba234f287b
ming-taro <hera002008@naver.com>
2022-05-13 13:45:01 +0900

    [MOVE] 버튼패널에서 작업하던 actionListener를 계산클래스로 옮김

--- Calculator/src/controller/ExpressionCalculation.java
+++ Calculator/src/controller/ExpressionCalculation.java
@@ -1,22 +1,45 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 import operationmanagement.ExpressionDTO;
 import view.CalculationButtonPanel;
 import view.CalculatorFrame;
 import view.ExpressionPanel;
 
-public class ExpressionCalculation {
+public class ExpressionCalculation implements ActionListener{
 	private ExpressionPanel expressionPanel;
 	private CalculationButtonPanel calculationButtonPanel;
 	private ExpressionDTO expression;
 	
 	public ExpressionCalculation() {
 		expressionPanel = new ExpressionPanel();
-		calculationButtonPanel = new CalculationButtonPanel(expressionPanel);
+		calculationButtonPanel = new CalculationButtonPanel(this);
 		CalculatorFrame calculatorFrame =  new CalculatorFrame(expressionPanel, calculationButtonPanel);
 	}
 	
 	public void setExpression(ExpressionDTO expression) {
 		this.expression = expression;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		JButton button = (JButton) event.getSource();
+		String buttonClicked = button.getText();
+
+		System.out.print(buttonClicked);
+		if(buttonClicked.charAt(0) >= '0' && buttonClicked.charAt(0) <= '9') {
+			expressionPanel.setInputLabel(buttonClicked);
+		}
+		else if(buttonClicked.equals("CE")) {
+			expressionPanel.setZero();
+		}
+		else if(buttonClicked.equals("C")) {
+			System.out.print(buttonClicked);
+			expressionPanel.removeInputLabel();
+		}
 	}
 }
--- Calculator/src/view/CalculationButtonPanel.java
+++ Calculator/src/view/CalculationButtonPanel.java
@@ -9,12 +9,13 @@ import javax.swing.JPanel;
 import operationmanagement.Constants;
 import operationmanagement.ButtonOnCalculator.Button;
 
-public class CalculationButtonPanel extends JPanel implements ActionListener{
+public class CalculationButtonPanel extends JPanel{
 	private JButton[] calculationButton;
-	private ExpressionPanel expressionPanel;
-	public CalculationButtonPanel(ExpressionPanel resultPanel){
+	private ActionListener buttonListener;
+	
+	public CalculationButtonPanel(ActionListener buttonListener){
 		calculationButton = new JButton[20];
-		this.expressionPanel = resultPanel;
+		this.buttonListener = buttonListener;
 		addButtonPanel();
 	}
 	
@@ -28,29 +29,11 @@ public class CalculationButtonPanel extends JPanel implements ActionListener{
 			index = button.getIndex();
 			calculationButton[index] = new JButton();
 			add(calculationButton[index]);
-			calculationButton[index].addActionListener(this);     //버튼클릭이벤트 연결
+			calculationButton[index].addActionListener(buttonListener);     //버튼클릭이벤트 연결
 			calculationButton[index].setText(button.getSymbol());   //버튼이름 설정
 			calculationButton[index].setFont(font);  
 			calculationButton[index].setFocusPainted(false);
 			calculationButton[index].setContentAreaFilled(false);
-		}
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent event) {
-		JButton button = (JButton) event.getSource();
-		String buttonClicked = button.getText();
-
-		System.out.print(buttonClicked);
-		if(buttonClicked.charAt(0) >= '0' && buttonClicked.charAt(0) <= '9') {
-			expressionPanel.setInputLabel(buttonClicked);
-		}
-		else if(buttonClicked.equals(calculationButton[0].getText())) {
-			expressionPanel.setZero();
-		}
-		else if(buttonClicked.equals(calculationButton[1].getText())) {
-			System.out.print(buttonClicked);
-			expressionPanel.removeInputLabel();
 		}
 	}
 
