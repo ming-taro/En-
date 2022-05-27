a00dd7ac0907b632140ff68d11119054a13743f8
ming-taro <hera002008@naver.com>
2022-05-27 18:21:55 +0900

    [ADD] 'help'입력시 명령어 설명 출력

--- CMD/src/controller/CmdManagement.java
+++ CMD/src/controller/CmdManagement.java
@@ -2,34 +2,51 @@ package controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import view.CommandUsage;
+import view.Help;
 import view.Start;
 
 public class CmdManagement {
 	private CommandUsage commandUsage;
+	private Help helpCommand;
 	
 	public CmdManagement() {
 		commandUsage = new CommandUsage();
+		helpCommand = new Help();
+	}
+	public void executeCommand(String command) {
+		command = command.toLowerCase();
+		
+		switch(command) {
+		case "help":
+			helpCommand.execute();
+		}
 	}
 	public void start(){
 		Start start = new Start();
-		start.printWindowVersion();
-		commandUsage.printDirUsage();
+		String command;
 		
-		//inputWord();
+		start.printWindowVersion();
+		
+		while(true) {
+			command = inputWord();
+			executeCommand(command);
+		}
 	}
-	public void inputWord(){
+	public String inputWord(){
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String str = "";
+		String word = "";
 		
 		try {
-			str = reader.readLine();
+			word = reader.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
+			System.out.println(e);
 		}
 		
-		System.out.println(str);
+		return word;
 	}
 }
