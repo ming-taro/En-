4554d7a375e0394bf9483664909cc55c8a56dbab
ming-taro <hera002008@naver.com>
2022-05-29 22:05:42 +0900

    [ADD] copy -> 컴퓨터에 존재하는 두 개의 파일 경로를 올바르게 입력한 경우 파일 복사기능 수행

--- CMD/src/controller/Copy.java
+++ CMD/src/controller/Copy.java
@@ -5,10 +5,90 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
+import utility.Constants;
+
 public class Copy {
-	private void copyfile(String pathOfFileToCopy, String pathOfFileToSave) {
-		 File fileToCopy = new File(pathOfFileToCopy);       
-		 File fileToSave = new File(pathOfFileToSave);   
+	private String firstFilePath;
+	private String secondFilePath;
+	
+	public String RemoveQuotationMark(String commandToChange, int currentPosition) {
+		StringBuilder command = new StringBuilder();
+		
+		command.append(commandToChange);
+		
+		if(command.charAt(currentPosition - 1) == '\\') {
+			command.deleteCharAt(currentPosition);  //첫 번째 큰따옴표 삭제
+		}
+		else {
+			return commandToChange;
+		}
+		
+		for(int index = currentPosition; index < command.length(); index++) {
+			if(command.charAt(index) == '"' 
+				&& (command.charAt(index + 1) == '\\' 
+					|| command.charAt(index + 1) == '.')) {
+				command.deleteCharAt(index);    //두 번째 큰따옴표 삭제
+				return command.toString();
+			}
+		}
+		System.out.println("<>" + commandToChange);
+		return commandToChange;
+	}
+	private int getIndexOfPointThatSeparatesFilePath(String command) {
+		if(command.indexOf("C:") == -1){              //ex: copy a.txt b.txt
+			return command.indexOf(" ");
+		}
+		if(command.indexOf("C:", 2) != -1) {          //ex: copy c:\\users\\a.txt c:\\users\\b.txt
+			return command.indexOf("C:", 2) - 1;      //ex: copy a.txt c:\\users\\b.txt
+		}
+		if(command.indexOf("C:", 2) == -1) {          //ex: copy c:\\users\\a.txt b.txt
+			return command.lastIndexOf(" ");
+		}
+		return -1;
+	}
+	private void executeCommand(String command) {
+		int index = 0;
+		int beginIndex = -1, endIndex = -1;
+		
+		while(index < command.length()) {
+			if(command.charAt(index) == '"') {                  //cmd에서는 빈칸이 있는 경우 큰따옴표로 묶여있어야 인식을 함 -> 입력받을때는 빈칸이 있는 경우 큰따옴표로 묶도록 함
+				command = RemoveQuotationMark(command, index);  //자바의 files.copy()는 빈칸이 있더라도 큰따옴표가 없어야 올바르게 인식 -> copy를 실행하기 위해 큰따옴표를 지워줌
+			}
+			index++;
+		}
+		if(command.indexOf('"') != -1) {                        //지워지지 않은 따옴표가 남아있는 경우 -> 유효하지 않은 파일 경로
+			System.out.println("지정된 파일을 찾을 수 없습니다.");
+			return;
+		}
+		
+		endIndex = getIndexOfPointThatSeparatesFilePath(command);  //첫 번째 파일의 경로정보가 끝나는 지점의 인덱스 값 
+		beginIndex = endIndex + 1;                                 //두 번째 파일의 경로정보가 시작되는 지점의 인덱스 값
+		
+		firstFilePath = command.substring(0, endIndex).trim();
+		secondFilePath = command.substring(beginIndex).trim();
+	}
+	private boolean isFileExisting(String filePath) {
+		File file = new File(filePath);
+		
+		if(file.exists()) {
+			System.out.println("I find the existFile.txt");
+			return Constants.IS_FILE_EXISTING;
+		}
+		System.out.println("No, there is not a no file.");
+		return !Constants.IS_FILE_EXISTING;
+	}
+	private boolean isValidPath() {
+		if(isFileExisting(firstFilePath) == !Constants.IS_FILE_EXISTING) {
+			return !Constants.IS_VALID_PATH;
+		}
+		if(isFileExisting(secondFilePath) == !Constants.IS_FILE_EXISTING) {
+			return !Constants.IS_VALID_PATH;
+		}
+		return Constants.IS_VALID_PATH;
+	}
+	private void copyfile() {
+		 File fileToCopy = new File(firstFilePath);       
+		 File fileToSave = new File(secondFilePath);   
 		  
 		 try {
 			Files.copy(fileToCopy.toPath(), fileToSave.toPath(), 
@@ -18,43 +98,21 @@ public class Copy {
 			e.printStackTrace();
 		} 
 	}
-	public String RemoveQuotationMark(String commandToChange, int currentPosition) {
-		StringBuilder command = new StringBuilder();
-		
-		command.append(commandToChange);
-		command.deleteCharAt(currentPosition);  //첫 번째 큰따옴표 삭제
-		
-		for(int index = currentPosition; index < command.length(); index++) {
-			if(command.charAt(index) == '"') {
-				command.deleteCharAt(index);    //두 번째 큰따옴표 삭제
-				break;
-			}
-		}
-		
-		return command.toString();
-	}
-	private void executeCommand(String command) {
-		int index = 0;
-		
-		while(index < command.length()) {
-			//cmd에서는 빈칸이 있는 경우 큰따옴표로 묶여있어야 인식을 함 -> 입력받을때는 빈칸이 있는 경우 큰따옴표로 묶도록 함
-			//자바의 files.copy()는 빈칸이 있더라도 큰따옴표가 없어야 올바르게 인식 -> copy를 실행하기 위해 큰따옴표를 지워줌
-			if(command.charAt(index) == '"') {                 
-				command = RemoveQuotationMark(command, index); 
-			}
-			index++;
-		}
-		
-		/*String[] file = command.split("  ");
-		
-		System.out.println("<" + file[0] + ">" + "<" + file[1] + ">");
-		copyfile(file[0].trim(), file[1].trim());*/
-	}
 	public void execute(String path, String command) {
 		int beginIndex = command.indexOf(' ') + 1;
 		
 		command = command.substring(beginIndex).trim();    //copy 다음에 이어지는 명령문
 		
-		executeCommand(command);
+		executeCommand(command);                           //파일경로 구하기
+		
+		if(firstFilePath == null) return;                  //유효하지 않은 파일 경로
+		
+		if(isValidPath() == !Constants.IS_VALID_PATH) {    //유효하지 않은 파일 경로
+			System.out.println("지정된 파일을 찾을 수 없습니다.");
+			return;
+		}
+		System.out.println(firstFilePath);
+		System.out.println(secondFilePath);
+		copyfile();
 	}
 }
--- CMD/src/utility/Constants.java
+++ CMD/src/utility/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
 	public static final boolean IS_CORRECT_COMMAND_ENTERED = true; 
 	public static final boolean IS_START_WIDTH_CORRECT_COMMAND = true;
 	public static final boolean IS_STRING_MATCHED = true;
+	public static final boolean IS_ENCLOSED_IN_QUOTATION_MARK = true;
+	public static final boolean IS_FILE_EXISTING = true;
+	public static final boolean IS_VALID_PATH = true;
 }
