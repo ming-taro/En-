880b9fcdcaf64307f535a2f4f77f3fa550f0ed79
ming-taro <hera002008@naver.com>
2022-05-29 17:31:14 +0900

    [ADD] files.copy()실행시 파일경로의 큰따옴표를 삭제한 후 실행

--- CMD/src/controller/Copy.java
+++ CMD/src/controller/Copy.java
@@ -11,17 +11,44 @@ public class Copy {
 		 File fileToSave = new File(pathOfFileToSave);   
 		  
 		 try {
-			Files.copy(fileToCopy.toPath(), fileToSave.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(fileToCopy.toPath(), fileToSave.toPath(), 
+					StandardCopyOption.REPLACE_EXISTING);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 	}
+	public String RemoveQuotationMark(String commandToChange, int currentPosition) {
+		StringBuilder command = new StringBuilder();
+		
+		command.append(commandToChange);
+		command.deleteCharAt(currentPosition);  //첫 번째 큰따옴표 삭제
+		
+		for(int index = currentPosition; index < command.length(); index++) {
+			if(command.charAt(index) == '"') {
+				command.deleteCharAt(index);    //두 번째 큰따옴표 삭제
+				break;
+			}
+		}
+		
+		return command.toString();
+	}
 	private void executeCommand(String command) {
-		String[] file = command.split("  ");
+		int index = 0;
+		
+		while(index < command.length()) {
+			//cmd에서는 빈칸이 있는 경우 큰따옴표로 묶여있어야 인식을 함 -> 입력받을때는 빈칸이 있는 경우 큰따옴표로 묶도록 함
+			//자바의 files.copy()는 빈칸이 있더라도 큰따옴표가 없어야 올바르게 인식 -> copy를 실행하기 위해 큰따옴표를 지워줌
+			if(command.charAt(index) == '"') {                 
+				command = RemoveQuotationMark(command, index); 
+			}
+			index++;
+		}
+		
+		/*String[] file = command.split("  ");
 		
 		System.out.println("<" + file[0] + ">" + "<" + file[1] + ">");
-		copyfile(file[0].trim(), file[1].trim());
+		copyfile(file[0].trim(), file[1].trim());*/
 	}
 	public void execute(String path, String command) {
 		int beginIndex = command.indexOf(' ') + 1;
