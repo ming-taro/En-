bd0a667dd9574efab49549c1425fd145a9b47df7
ming-taro <hera002008@naver.com>
2022-06-04 02:39:06 +0900

    [ADD] 로그인 후 사용자 모드 패널로 전환, 로그아웃 버튼 클릭시 로그아웃 여부를 묻는 팝업창을 띄움

--- SignUp/src/ui/PanelSwitcher.java
+++ SignUp/src/ui/PanelSwitcher.java
@@ -25,7 +25,7 @@ import utility.Constants;
 public class PanelSwitcher extends JFrame implements ActionListener{
 	private ProfileDAO profileDAO;
 	private PanelFactory panelFactory;
-	private UICreator panel;
+	private UICreator panelCurrentlyInUse;
 	
 	public PanelSwitcher() {
 		panelFactory = new PanelFactory();
@@ -35,6 +35,7 @@ public class PanelSwitcher extends JFrame implements ActionListener{
 		
 		setComponent();
 	}
+	
 	public void setComponent() {
 		setSize(Constants.WIDTH, Constants.HEIGHT);
 		setTitle("StardewValley");
@@ -48,6 +49,7 @@ public class PanelSwitcher extends JFrame implements ActionListener{
 		
 		addPanel(Constants.MAIN_PANEL);  //frame 첫 실행시 메인화면으로 시작
 	}
+	
 	private void switchPanel(JPanel panel) {
 		getContentPane().removeAll();
 		getContentPane().add(panel, BorderLayout.CENTER);
@@ -56,23 +58,24 @@ public class PanelSwitcher extends JFrame implements ActionListener{
 	}
 	
 	private void addPanel(String panelName) {
-		panel = panelFactory.getPanel(panelName);
-		panel.setActionListener(this);
+		panelCurrentlyInUse = panelFactory.getPanel(panelName);
+		panelCurrentlyInUse.setActionListener(this);
 		
 		getContentPane().removeAll();
-		getContentPane().add((Component) panel, BorderLayout.CENTER);
+		getContentPane().add((Component) panelCurrentlyInUse, BorderLayout.CENTER);
 		getContentPane().revalidate();
 		getContentPane().repaint();
 	}
 
 	private void setSignUp() {
 		Profile profile = 
-				((SignUpPanel) panel).getProfile();   //회원가입 화면에서 입력한 회원정보를 가져옴
+				((SignUpPanel) panelCurrentlyInUse).getProfile();   //회원가입 화면에서 입력한 회원정보를 가져옴
 		String errorMessage = 
-				((SignUpPanel) panel).isProfileEnteredCorrectly(profile) ;   //가져온 데이터 중 비어있거나 양식에 맞지 않은 입력 검사
+				((SignUpPanel) panelCurrentlyInUse).isProfileEnteredCorrectly(profile) ;   //가져온 데이터 중 비어있거나 양식에 맞지 않은 입력 검사
 																			     //해당 항목에 대한 오류 메세지를 리턴받음
 		if(errorMessage.equals("") == !Constants.IS_MATCH) {
-			JOptionPane.showMessageDialog(null, errorMessage, "입력 오류", JOptionPane.WARNING_MESSAGE);
+			JOptionPane.showMessageDialog(null, errorMessage, 
+					"입력 오류", JOptionPane.WARNING_MESSAGE);
 			return;
 		}
 		
@@ -86,8 +89,10 @@ public class PanelSwitcher extends JFrame implements ActionListener{
 	}
 	
 	private void setLogIn() {
-		String id = ((MainPanel) panel).getIdTextField().getText();
-		String password = ((MainPanel) panel).getPaswwordTextField().getText();
+		String id = 
+				((MainPanel) panelCurrentlyInUse).getIdTextField().getText();
+		String password = 
+				((MainPanel) panelCurrentlyInUse).getPaswwordTextField().getText();
 		
 		if(id.equals("")) {
 			JOptionPane.showMessageDialog(null, "아이디를 입력해주세요",  //아이디를 입력하지 않은 경우 -> 팝업 메세지
@@ -107,12 +112,11 @@ public class PanelSwitcher extends JFrame implements ActionListener{
 			JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요.",  
 					"로그인 오류", JOptionPane.WARNING_MESSAGE);
 		}
-		
 	}
 	
 	private void manageMainPanel(ActionEvent event) {
-		
-		if(event.getSource() == ((MainPanel) panel).getSignUpButton()) {        //메인화면에서 회원가입버튼 클릭 -> 회원가입 창으로 이동
+		if(event.getSource() == 
+				((MainPanel) panelCurrentlyInUse).getSignUpButton()) {        //메인화면에서 회원가입버튼 클릭 -> 회원가입 창으로 이동
 			addPanel(Constants.SIGN_UP_PANEL);
 		}
 		else {
@@ -121,33 +125,43 @@ public class PanelSwitcher extends JFrame implements ActionListener{
 	}
 	
 	private void manageSignUpPanel(ActionEvent event) {
-		
-		if(event.getSource() == ((SignUpPanel) panel).getBackButton()){    //회원가입화면에서 뒤로가기 버튼 클릭 -> 메인화면으로 이동 
+		if(event.getSource() == 
+				((SignUpPanel) panelCurrentlyInUse).getBackButton()){    //회원가입화면에서 뒤로가기 버튼 클릭 -> 메인화면으로 이동 
 			addPanel(Constants.MAIN_PANEL);
 		}
-		else { //회원가입화면에서 '회원가입'버튼클릭
-			setSignUp();
+		else { 
+			setSignUp();  //회원가입화면에서 '회원가입'버튼클릭
 		}
+	}
+	
+	private void manageUserModePanel(ActionEvent event) {
+		int logout = JOptionPane.showConfirmDialog(null, "로그아웃 하시겠습니까?", 
+				"로그아웃", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+		
+		if(logout == Constants.YES_OPTION) {   //회원모드에서 로그아웃 클릭 -> 팝업창에서 yes or no 선택
+			addPanel(Constants.MAIN_PANEL);
+		}
+		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		String className = panel.getClass().getName();
-		className = className.substring(className.lastIndexOf(".") + 1);
+		String className = panelCurrentlyInUse.getClass().getName();
+		className = className.substring(className.lastIndexOf(".") + 1);   //class이름으로 현재 fram에 올려진 panel을 구별
 		
-		switch(className) {
+		switch(className) {                       //추출한 클래스 이름으로 해당 클래스의 이벤트 실행(클래스가 null인지 체크하지 않아도 됨)
 		
 		case Constants.MAIN_PANEL:
 			manageMainPanel(event);
+			break;
+		case Constants.SIGN_UP_PANEL:
+			manageSignUpPanel(event);
 			break;
 		case Constants.SIGN_UP_COMPLETION_PANEL:  //회원가입 완료화면에서 로그인 버튼 클릭 -> 메인화면으로 이동
 			addPanel(Constants.MAIN_PANEL);
 			break;
 		case Constants.USER_MODE_PANEL:           //회원모드에서 로그아웃 버튼 클릭 -> 메인화면으로
-			addPanel(Constants.MAIN_PANEL);
-			break;
-		case Constants.SIGN_UP_PANEL:
-			manageSignUpPanel(event);
+			manageUserModePanel(event);
 			break;
 		}
 	}
